bd5dd2e6244baa09e43cb317486a97c0df22fd50
horothesun <horothesun@gmail.com>
2022-09-20 00:09:23 +0100

    Postgres version pinned

--- scripts/test_db_up.sh
+++ scripts/test_db_up.sh
@@ -2,6 +2,7 @@
 
 export PGPASSWORD="test_pwd"
 
+POSTGRES_VERSION="14.5"
 TEST_DEFAULT_USER="postgres"
 TEST_DB_HOST="localhost"
 TEST_CONTAINER="test-postgres"
@@ -15,7 +16,7 @@ docker run \
   --name "${TEST_CONTAINER}" \
   --publish "${TEST_DB_PORT}:${TEST_DB_PORT}" \
   --env POSTGRES_PASSWORD="${PGPASSWORD}" \
-  postgres
+  "postgres:${POSTGRES_VERSION}"
 
 echo "Waiting for Postgres to be ready..."
 sleep 3
