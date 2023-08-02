b61ef960d81a4247c195f8102761d08fb405330f
horothesun <horothesun@gmail.com>
2023-08-02 01:02:07 +0100

    PostgresQueueClient trait

--- root/src/main/scala/com/horothesun/postgresqueue/PostgresQueueClient.scala
+++ root/src/main/scala/com/horothesun/postgresqueue/PostgresQueueClient.scala
@@ -1,5 +1,15 @@
 package com.horothesun.postgresqueue
 
-trait PostgresQueueClient
+import cats.effect.IO
+import Models._
+
+trait PostgresQueueClient {
+  def createQueue(queueName: QueueName): IO[Unit]
+  def createQueue(queueName: QueueName, queueVisibilityTimeout: QueueVisibilityTimeout): IO[Unit]
+  def getQueueVisibilityTimeout(queueName: QueueName): IO[Option[QueueVisibilityTimeout]]
+  def updateQueueVisibilityTimeout(queueName: QueueName, queueVisibilityTimeout: QueueVisibilityTimeout): IO[Unit]
+  def deleteQueue(queueName: QueueName): IO[Unit]
+  def publishMessage(messageBody: MessageBody): IO[Unit]
+}
 
 object PostgresQueueClient {}
